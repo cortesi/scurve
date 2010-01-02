c61dc8db68cfea46cde919f7c92367df2a8618d5
Aldo Cortesi <aldo@nullcube.com>
2010-01-02 16:35:24 +1300

    Include a progress bar library from another project of mine.

---
+++ test/test_progress.py
@@ -0,0 +1,38 @@
+import scurve.progress as progress
+import libpry
+import StringIO
+
+class uInplace(libpry.AutoTree):
+    def test_basic(self):
+        s = StringIO.StringIO()
+        c = progress.Inplace(stream=s)
+        assert s.getvalue() ==  ''
+        c.tick(10)
+        assert s.getvalue() ==  '\r10'
+        c.tick(10000)
+        assert s.getvalue() ==  '\r10\r10000'
+        c.inject("foo")
+        c.clear()
+
+    def test_nostream(self):
+        c = progress.Inplace(stream=None)
+        c.tick(10)
+        c.clear()
+
+
+class uProgress(libpry.AutoTree):
+    def test_basic(self):
+        s = StringIO.StringIO()
+        p = progress.Progress(100, stream=s)
+        p.tick(25)
+        assert p.prev == 0.25
+        p.tick(50)
+        assert p.prev == 0.5
+        p.full()
+        assert p.prev == 1.0
+
+
+tests = [
+    uInplace(),
+    uProgress()
+]
