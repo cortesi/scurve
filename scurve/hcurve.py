4e5fdffecf225a28362f09c4b6e739ff8dff8b69
Aldo Cortesi <aldo@nullcube.com>
2011-02-22 11:03:47 +1300

    Small code refactoring for hcurve.

--- scurve/hcurve.py
+++ scurve/hcurve.py
@@ -46,34 +46,53 @@ class Hcurve:
             raise IndexError
         return self.point(idx)
 
-    def xcor(self, i, n):
-        if i < 2:
+    def cor(self, d, i, n):
+        # Size of this sub-triangle
+        tsize = n**self.dimension/2
+        if i < 0:
             return 0
+        elif i < d + 1:
+            return d
+        elif i >= tsize:
+            return n - self.cor(d, i - tsize, n) - 1
         else:
-            if i >= n*n/2:
-                return n - self.xcor((i-n*n/2), n) - 1
-            else:
-                x = 8*i/n/n
-                if x == 0: return self.xcor(i, n/2)
-                if x == 1: return self.xcor(n*n/4-1-i, n/2)
-                if x == 2: return n/2 - self.xcor(3 * n*n/8-1-i, n/2) - 1
-                if x == 3: return n/2 + self.xcor(i- 3 * n*n/8, n/2)
-        return 0
+            # Which of the four sub-triangles of this triangle are we in?
+            x = 4*i/tsize
+            f = 1 if i+1 == d else 0
+            if x == 0: return self.cor(d, i, n/2) 
+            if x == 1: return self.cor(d, tsize/2 - 1 - i, n/2) - 0
+            if x == 2: return n/2 - self.cor(d, 3 * tsize/4 - 1 - i, n/2) - 1
+            if x == 3: return n/2 + self.cor(d, i - 3 * tsize/4, n/2) - 0
+
+    def xcor(self, i, n):
+        # Size of this sub-triangle
+        tsize = n**self.dimension/2
+        if i < 1:
+            return 0
+        elif i >= tsize:
+            return n - self.xcor(i - tsize, n) - 1
+        else:
+            # Which of the four sub-triangles of this triangle are we in?
+            x = 4*i/tsize
+            f = 0
+            if x == 0: return self.xcor(i, n/2)
+            if x == 1: return self.xcor(tsize/2 - 1 - i, n/2)
+            if x == 2: return n/2 - self.xcor(3 * tsize/4 - 1 - i, n/2) - 1
+            if x == 3: return n/2 + self.xcor(i - 3 * tsize/4, n/2)
                 
     def ycor(self, i, n):
-        if (i < 2):
+        tsize = n**self.dimension/2
+        if i < 2:
             return i
+        elif i >= tsize:
+            return n - self.ycor(i - tsize, n) - 1
         else:
-            if (i >= n*n/2):
-                return n - self.ycor((i-n*n/2), n) - 1
-            else:
-                x = 8*i/n/n
-                if x == 0: return self.ycor(i, n/2)
-                if x == 1: return n - self.ycor(n*n/4-1-i, n/2) - 1
-                if x == 2: return n/2 + self.ycor(3 * n*n/8-1-i, n/2)
-                if x == 3: return n/2 + self.ycor(i- 3 * n*n/8, n/2)
-        return 0
+            x = 4*i/tsize
+            if x == 0: return self.ycor(i, n/2)
+            if x == 1: return n - self.ycor(tsize/2 - 1 - i, n/2) - 1
+            if x == 2: return n/2 + self.ycor(3 * tsize/4 - 1 - i, n/2)
+            if x == 3: return n/2 + self.ycor(i - 3 * tsize/4, n/2)
 
     def point(self, idx):
-        return [self.xcor(idx, self.size), self.ycor(idx, self.size)]
+        return [self.cor(0, idx, self.size), self.ycor(idx, self.size)]
 
